f46fed383d37aec28357303786b7a8a1aa2f4dde
badogan <basri.dogan.71@gmail.com>
2019-11-22 19:09:22 +0000

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,39 @@
 class Manager
 
+    attr_accessor :name, :department, :age, :employees, :age
+
+    @@all = []
+
+    def initialize(name:,department:,age:)
+        @name = name
+        @department = department
+        @age = age
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(name:,salary:)
+        new_employee = Employee.new(name:name, salary:salary, manager:self)
+    end
+
+    def employees
+        Employee.all.select {|employee| employee.manager == self}
+    end
+
+    def self.all_departments
+        Manager.all.map{|manager|manager.department}.uniq
+    end
+
+    def self.average_age
+        ages_array = Manager.all.map{|manager|manager.age}
+        ages_array.sum/ages_array.length
+    end
+
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,17 @@ require 'pry'
 
 
 #Test your code here
+m1 = Manager.new(name:"Basri",department:"Dept1",age:30)
+m2 = Manager.new(name:"John",department:"Dept2",age:41)
+m3 = Manager.new(name:"Basri2",department:"Dept11111",age:300)
+m4 = Manager.new(name:"John2",department:"Dept222222",age:411)
+e1 = Employee.new(name:"Arda - the Employee", salary:40000, manager:m1)
+e2 = Employee.new(name:"Another Employee", salary:49000,manager:m1)
+e3 = Employee.new(name:"Employee3", salary:31000,manager:m1)
+e4 = Employee.new(name:"Employee4", salary:54000,manager:m1)
 
+m1.hire_employee(name:"New employee for m1",salary:32000)
+m2.hire_employee(name:"New employee for m2",salary:22000)
 
 binding.pry
 puts "done"
