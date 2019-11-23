133f8bfc3f0b119c8329efd785a4da224529237c
Matthew <matthew@matthews-mbp.home>
2019-11-23 20:32:16 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,21 @@
 class Employee
+    attr_reader :name
+    attr_accessor :salary, :manager_name
 
+    @@all = []
+
+    def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary
+        @manager_name = manager_name
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(num)
+        Employee.all.select{|employee| employee.salary > num}
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,37 @@
-class Manager
+require 'pry'
 
+class Manager
+    attr_reader :name, :department
+    attr_accessor :age
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self.name)
+    end
+
+    def employees
+        Employee.all.select{|employee| employee.manager_name == self.name }
+    end
+
+    def self.all_departments
+        Manager.all.map{|manager| manager.department}
+    end
+
+    def self.average_age
+        sum = 0
+        Manager.all.each{|manager| sum += manager.age}
+        sum.to_f / Manager.all.length
+    end
 end
